30c233633b50cc39da7190a68161fbf0431ec486
$udh!r Vaghel@ <54213144+SudhirVaghela@users.noreply.github.com>
2020-08-10 09:22:09 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -10,7 +10,7 @@ con = open('convo.txt')
 trainbot.train(con.readlines())
 
 print('start to chat.....')
-print('hey sudhir how r u  be na jaldi ho')
+print('hey sudhir')
 
 while True:
     user = input()
